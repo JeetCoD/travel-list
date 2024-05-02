3f5994e1024db1b565bc1755f352fe3bd4a1c2d3
Jeet Gajjar <94225096+JeetCoD@users.noreply.github.com>
2024-05-02 20:10:43 +0530

    First commit

---
+++ src/components/PackingList.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Items from "./Items";
+
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") {
+    sortedItems = items;
+  } else if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  } else if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Items
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Short by input order</option>
+          <option value="description">Short by description</option>
+          <option value="packed">Short by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear List</button>
+      </div>
+    </div>
+  );
+}
